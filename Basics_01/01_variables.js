97af754493aceb1b0f7ea88eb56e24e2e87f96f5
sarvesh227 <144058498+sarvesh227@users.noreply.github.com>
2025-03-09 05:50:34 +0000

    2nd Commit

---
+++ Basics_01/01_variables.js
@@ -0,0 +1,10 @@
+const account_id = 144556
+let account_email="Sarve@gmail.com"
+var account_password="12345"
+account_city="Ranchi"
+let account_State;
+/*
+Prefer Not to use Var
+*/
+
+console.table([account_id, account_email, account_password, account_city,'Abc',account_State])
