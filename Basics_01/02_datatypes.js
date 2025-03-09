4bf9ac48ce8291f84d5d378af08dc8b54f94c8f3
sarvesh227 <144058498+sarvesh227@users.noreply.github.com>
2025-03-09 06:41:46 +0000

    data types

---
+++ Basics_01/02_datatypes.js
@@ -0,0 +1,28 @@
+"use Strict"; // Treat all JS code as newer version
+//alert(3+3) // it will not run directly in Node js ... runs in browser
+
+console.log("SARVESH")
+
+let name = "Sarvesh"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//DATA TYPES
+//  number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// Object
+
+console.log(typeof age) // because age is just a number :)
+console.log(typeof name)
+
+console.log(typeof null) //  object
+console.log(typeof undefined) // undefined 
+
+
